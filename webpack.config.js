8acd7fddf9e7b9c1afcb8f5b2b80a72efd281433
jung <jung@saramin.co.kr>
2019-08-01 13:40:22 +0900

    package 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
+const webpack = require('webpack');
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const readline = require('readline');
+const cpu_number = require('os').cpus().length;
 
 module.exports = {
-    mode: 'development',
+    mode : 'production',
     entry: {
         app: ['@babel/polyfill', './lib/app.js']
     },
     output: {
         filename: '[name].js',
+        chunkFilename: '[name].chunk.js?v=[chunkhash]',
         path: path.resolve(__dirname, 'dist'),
         libraryTarget: 'var',
         library: 'EntryPoint',
@@ -17,9 +22,6 @@ module.exports = {
         compress: false,
         port: 8888
     },
-    plugins: [
-        new CleanWebpackPlugin(['dist'])
-    ],
     module: {
         rules: [{
             test: /\.js$/,
@@ -28,17 +30,40 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env']
-                    // presets: [
-                    //     ['env', {
-                    //         'targets': {
-                    //             'browsers': ['last 2 versions', 'ie >= 8']
-                    //         }
-                    //     }]
-                    // ]
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                'targets' : {
+                                    'browsers' : ['last 2 versions', 'ie >= 8']
+                                }
+                            }
+                        ]
+                    ],
+                    cacheDirectory: true
                 }
             }
         }]
     },
-    devtool: 'source-map'
+    optimization : {
+        minimizer: [
+            new TerserPlugin({
+                parallel : cpu_number - 1,
+                sourceMap: true,
+                cache : true
+            })
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.SourceMapDevToolPlugin({
+            filename : 'sourcemap/[name].js.map',
+            publicPath : __dirname + '/dist/'
+        }),
+        new webpack.ProgressPlugin((percentage, message) => {
+            readline.cursorTo(process.stdout, 0);
+            readline.clearLine(process.stdout,0);
+            process.stdout.write(`\x1b[1m\x1b[36m${(percentage * 100).toFixed()}% \x1b[0m\x1b[2m\x1b[36m${message}`);
+        })
+    ]
 };
